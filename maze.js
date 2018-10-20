7360164aa206ab3ab46b1700b6d3a44b57b8a862
joevel24 <joevel.edwards@yahoo.com>
2018-10-20 16:35:20 -0500

    lab 4

---
+++ maze.js
@@ -0,0 +1,39 @@
+function changeColor (){
+  document.getElementById("boundary1").classList.add("youlose");
+}
+
+changeColor();
+
+
+function changeBoundary (){
+var bound = document.querySelectorAll(".boundary"), i;
+
+for (i = 0; i < bound.length; i++) {
+  bound[i].classList.add("youlose");
+}
+}
+
+changeBoundary();
+
+//function showMsg(textVal)
+//{
+//document.getElementById("end").innerHTML=textVal;
+//}
+//function clearMsg()
+//{
+//document.getElementById("end").innerHTML="";
+//}
+
+
+function winAlert() {
+    
+    alert("You win!");
+    
+}
+
+/*function startSquare (changeBoundary()) {
+    
+    if (changeBoundary() == "true"){
+        
+    }
+}*/
