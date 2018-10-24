13017a7dcc643da9fb79d03f52aa163e3ee07825
joevel24 <joevel.edwards@yahoo.com>
2018-10-23 22:48:07 -0500

    lab4 official

--- maze.js
+++ maze.js
@@ -1,45 +1,69 @@
-function restart() {
-	document.location.href = "";
+let boundary;
+let condition = false;
+let objective;
+let testwin = false;
+let leave;
+let start;
+
+
+window.onload = function() {
+	_status = document.getElementById("status");
+	objective = document.getElementById("end");
+	objective.onmouseover = youWin;
+	start = document.getElementById("start");
+	leave = document.getElementById("maze");
+	leave.onmouseleave = outside;
+	start.onclick = restart;
+	boundary = document.querySelectorAll(".boundary");
+	for (var i = 0; i < boundary.length - 1; i++) {
+		boundary[i].onmouseover = changeBoundary;
+	}
 }
 
-function changeColor (){
+/*function changeColor() {
   document.getElementById("boundary1").classList.add("youlose");
-}
-
-changeColor();
-
-
-function changeBoundary (){
-var bound = document.querySelectorAll(".boundary"), i;
-
-for (i = 0; i < bound.length; i++) {
-  bound[i].classList.add("youlose");
-}
-}
-
-changeBoundary();
-
-//function showMsg(textVal)
-//{
-//document.getElementById("end").innerHTML=textVal;
-//}
-//function clearMsg()
-//{
-//document.getElementById("end").innerHTML="";
-//}
-
-
-function winAlert() {
-		alert("You win!");
-}
-
-function youWin(){
-  document.getElementById("status").innerHTML = "You Win!";
-}
-
-/*function startSquare (changeBoundary()) {
-    
-    if (changeBoundary() == "true"){
-        
-    }
+  document.getElementById("status").innerHTML = "You Lose!";
 }*/
+
+//changeColor();
+
+
+function changeBoundary() {
+	if(!testwin) {
+		if(!condition) {
+			condition = true;
+	boundary = document.querySelectorAll(".boundary");
+	_status.innerHTML = "You Lose!";
+	for (var i = 0; i < boundary.length - 1; i++) {
+		boundary[i].classList.add("youlose");
+		}
+		}
+	}
+	
+}
+
+//changeBoundary();
+
+function youWin() {
+	if(!condition) {
+		testwin = true;
+		_status.innerHTML = "You Win!";
+	}
+}
+
+function restart() {
+	condition = false;
+	testwin = false;
+	boundary = document.querySelectorAll(".boundary");
+	for (var i = 0; i < boundary.length - 1; i++) {
+		boundary[i].className = "boundary";
+	}
+	//document.location.href = "";
+	
+}
+
+function outside() {
+	if (!testwin) {
+		changeBoundary();
+	}
+}
